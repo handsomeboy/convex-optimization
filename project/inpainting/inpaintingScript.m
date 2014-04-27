bd3bc4679541ad7b693b18ea3d37da6cd9a54237
Michèle Wyss <michele_wyss@students.unibe.ch>
2014-04-27 18:32:57 +0200

    Cleaned up code

--- project/inpainting/inpaintingScript.m
+++ project/inpainting/inpaintingScript.m
@@ -1,43 +1,25 @@
 
 clear all;
-% close all;
+close all;
 clc;
 
 im = im2single(rgb2gray(imread('Figures/hat.jpg')));
 im = imresize(im, [250 215]); % not needed for grumpycat.jpeg
+
 % mask
 omega = ones(size(im));
 omega(175:189,11:114) = 0;
 omega(31:65,166:194) = 0;
 
 % create input image
-size(im)
-size(omega)
-% imtool(omega);
 g = im.*omega;
 
+% apply inpainting method
+lambda = 53;
+uG = inpainting_MicheleWyss(g,omega,lambda);
 
-lambda = 500;
-
-% for lambda = 10:10:1990
-[uG, cost] = inpainting_MicheleWyss(g,omega,lambda);
-% ssd = sum(sum((uG-im).^2));
-%     ssd(lambda) = sum(sum((uG-im).^2));
-%     lambda
-% end
-% vec = [10:10:1990];
-% plot(vec,ssd(10:10:1990));
-
-
-% figure;
-% hold on;
-% plot(ssd); title('Grumpy cat'); xlabel('\lambda'); ylabel('SSD');
-% [min,idx] = min(ssd);
-% plot(idx,0:100);
-
-% figure;
-% disp = [uG, (uG-im).^2; ...
-%         im, g];
-% imshow(disp);
-% plot(cost_la);
-imtool(uG)
+% display results
+figure;
+disp = [uG, (uG-im).^2; ...
+        im, g];
+imshow(disp);
