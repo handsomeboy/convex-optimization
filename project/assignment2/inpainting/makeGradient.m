8ff5e3491ad6d1976e0822a54d827f7a304bf019
Michèle Wyss <michele_wyss@students.unibe.ch>
2014-05-25 16:55:19 +0200

    Added commented out try to use forward differences - to show that I really tried... :-P

--- project/assignment2/inpainting/makeGradient.m
+++ project/assignment2/inpainting/makeGradient.m
@@ -3,17 +3,20 @@ function [ grad ] = makeGradient( x )
 %
 % output: grad: a MxNx2 matrix containing the gradient components in dx and
 % dy.
-%
-% Comment: I tried to use only forward differences, but the whole thing
-% failed because in this case the gradients kind of "shifted" away. Is this
-% a general problem, or should I have tried something smarter? Matlab's
-% gradient function produces a centered difference approximation of the
-% gradient, but I actually thought that this is not suited for such
-% applications...
-[grad_1 grad_2] = gradient(x);
-grad = zeros(size(x));
-grad(:,:,1 ) = grad_1;
-grad(:,:,2 ) = grad_2;
 
+% Comment: I tried to use only forward differences, but the whole thing
+% failed because in this case the gradients kind of "shifted" away.
+%    grad_1 = x([2:end end],1:end) - x(1:end,1:end);
+%    grad_2 = x(1:end,[2:end end]) - x(1:end,1:end);
+
+% So I used centered differences, but this is actually not well suited for
+% such applications...
+   grad_1 = ((x([2:end end],1:end) - x(1:end,1:end)) + (x(1:end,1:end) - x([1 1:end-1],1:end)))./2;
+   grad_2 = (x(1:end, [2:end end]) - x(1:end, 1:end) + x(1:end, 1:end) - x(1:end, [1 1:end-1]))./2;
+
+   grad = zeros([size(x) 2]);
+   
+   grad(:,:,1) = grad_1;
+   grad(:,:,2) = grad_2;
 end
 
