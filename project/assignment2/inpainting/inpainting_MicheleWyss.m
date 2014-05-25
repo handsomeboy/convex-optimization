1fae54c83ce93838d4dee5644c2cfe538b48c79c
Michèle Wyss <michele_wyss@students.unibe.ch>
2014-05-25 15:49:22 +0200

    Removed some debug-if's

--- project/assignment2/inpainting/inpainting_MicheleWyss.m
+++ project/assignment2/inpainting/inpainting_MicheleWyss.m
@@ -6,49 +6,34 @@ function [u] = inpainting_MicheleWyss(g,omega,lambda)
 % output: u: inpainted image
 
 % initialize
-tau = 0.1;
-sigma = 1/0.08;
+tau = 0.01;
+sigma = 10;
 
 theta = 0.5;
 
-x = g;
-y = zeros(size(g,1), size(g,2), 2);
+x = zeros(size(g));
+y = zeros([size(g) 2]);
 x_tilde = x;
-debug_pos = 1
 
 %iterations
-iter = 100;
+iter = 500;
 h = waitbar(0,'Iterate...');
 for i = 1:iter
    waitbar(i/iter);
+   
    grad_x = makeGradient(x_tilde);
-     
-   if (i == debug_pos)
-       imtool(grad_x(:,:,1));
-       imtool(grad_x(:,:,2));
-       imtool(grad_x(:,:,1) + grad_x(:,:,2));
-   end
    
    y_term = y + sigma*grad_x;
    
    norm = sqrt(y_term(:,:,1).^2 + y_term(:,:,2).^2);
    y(:,:,1) = y_term(:,:,1)./max(1, norm);
    y(:,:,2) = y_term(:,:,2)./max(1, norm);
-   
+
    x_old = x;
    div_y = makeDiv(y);
-
    x = (x + tau*div_y + tau*lambda*(omega.*g)) ./ (1 + tau*lambda*omega);
-   if (i == debug_pos)
-       diff = abs(x-x_old);
-       imtool(diff);
-   end
+
    x_tilde = x + theta * (x - x_old);
-   
-   if (i == debug_pos)
-       imtool(x_tilde);
-   end
-   
 end
 close(h);
 u = x_tilde;
