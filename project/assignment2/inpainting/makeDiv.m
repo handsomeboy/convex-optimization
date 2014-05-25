a5ad5c93e5113f39a67ac70ffaad635a8b008552
Michèle Wyss <michele_wyss@students.unibe.ch>
2014-05-25 15:50:04 +0200

    Added some comments to divergence-maker

--- project/assignment2/inpainting/makeDiv.m
+++ project/assignment2/inpainting/makeDiv.m
@@ -1,10 +1,16 @@
-function [ div ] = makeDiv( x )
+function [div] = makeDiv(x)
+% input:    x is a MxNx2-matrix. In the case of this project, 
+%           it contains the gradient of an MxN gray scale image.
+%
+% output:   div is a MxN-matrix containing the divergence. 
+%           In the case of this project it contains 
+%           d^2/dx^2 + d^2/dy^2
    term1 = makeGradient(x(:,:,1));
    term1 = term1(:,:,1);
-   
+      
    term2 = makeGradient(x(:,:,2));
    term2 = term2(:,:,2);
-   
-   div = term1 + term2;   
+
+   div = term1 + term2; 
 end
 
