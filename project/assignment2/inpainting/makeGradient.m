e553ec3137a9ef4befcd70257b33f9c9bb74d615
Michèle Wyss <michele_wyss@students.unibe.ch>
2014-05-25 15:52:08 +0200

    Switched to Matlab's own gradient-function because forward difference scheme caused stuff to fuck up due to shiftings...

--- project/assignment2/inpainting/makeGradient.m
+++ project/assignment2/inpainting/makeGradient.m
@@ -1,23 +1,19 @@
 function [ grad ] = makeGradient( x )
-   %% construct bigger x_tilde image with mirrored boundaries
-   flipH = flipdim(x,2);
-   bigH = [flipH(:,end) x];
-   
-   flipV = flipdim(bigH,1);
-   x_big = [ flipV(end,:); bigH];
-   
-   %% construct gradient based on the enlarged image uNew (big enough s.t. all the necessary indices are available)
-   grad_1 = x_big(2:end,2:end) - x_big(1:end-1,2:end);
-   grad_2 = x_big(2:end,2:end) - x_big(2:end,1:end-1);
-   
-   grad = zeros(size(x,1), size(x,2), 2);
-   
-   grad(:,:,1) = grad_1;
-   grad(:,:,2) = grad_2;
-   %grad = grad + 1e-6;
-   %grad = grad.^(0.5);
-   %grad = grad(2:end-1,2:end-1);
-   
-   %grad = div;
+% input: x is a MxN gray scale image
+%
+% output: grad: a MxNx2 matrix containing the gradient components in dx and
+% dy.
+%
+% Comment: I tried to use only forward differences, but the whole thing
+% failed because in this case the gradients kind of "shifted" away. Is this
+% a general problem, or should I have tried something smarter? Matlab's
+% gradient function produces a centered difference approximation of the
+% gradient, but I actually thought that this is not suited for such
+% applications...
+[grad_1 grad_2] = gradient(x);
+grad = zeros(size(x));
+grad(:,:,1 ) = grad_1;
+grad(:,:,2 ) = grad_2;
+
 end
 
